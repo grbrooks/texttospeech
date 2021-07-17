0e0fbee315edfea7178545446f61eb0f5a1b12fb
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-17 12:54:33 +0100

    --- Auto-Git Commit ---

--- textToSpeech.js
+++ textToSpeech.js
@@ -2,6 +2,13 @@ let speech = new SpeechSynthesisUtterance();
 
 speech.lang = "en";
 let voices = [];
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+
+    speech.voice = voices[0];
+    let voiceSelect = document.querySelector("#voices")
+    voices.forEach((voice, i) => (voiceSelect.options[i] == new Option(voice.name, i)));
+}
 document.querySelector("#talk").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
 });
