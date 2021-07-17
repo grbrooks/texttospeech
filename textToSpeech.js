d1b70bf20db81e0103891be34ad09ffe1d4dc1e8
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-17 12:24:14 +0100

    --- Auto-Git Commit ---

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,2 +1,17 @@
 let speech = new SpeechSynthesisUtterance();
-document.querySelector("#talk").addEventListener("click", () => { speech.text = document.querySelector("textarea").value; })
+
+speech.lang = "en";
+let voices = [];
+document.querySelector("#talk").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+});
+document.querySelector("#rate").addEventListener("input", () => {
+    // Get rate Value from the input
+    const rate = document.querySelector("#rate").value;
+
+    // Set rate property of the SpeechSynthesisUtterance instance
+    speech.rate = rate;
+
+    // Update the rate label
+    document.querySelector("#rate-label").innerHTML = rate;
+});
